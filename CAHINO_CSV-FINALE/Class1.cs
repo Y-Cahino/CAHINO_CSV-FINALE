136224ec72e7778a01dbf6cbf28705cfcfb8cf11
Yan Nascimento <yanzebedeu@hotmail.com>
2023-05-31 01:18:29 +0200

    funzione search abilitata

--- CAHINO_CSV-FINALE/Class1.cs
+++ CAHINO_CSV-FINALE/Class1.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace CAHINO_CSV_FINALE
 {
@@ -32,5 +33,33 @@ namespace CAHINO_CSV_FINALE
             sr.Close();
             sw.Close();
         }
+    }
+    public void search(string filename, char delim, string name)
+    {
+        int pos;
+        StreamReader sr = new StreamReader(filename);
+        string s;
+        name = name.ToUpper();
+        int i = 0;
+        int con = -1;
+        while ((s = sr.ReadLine()) != null)
+        {
+            string[] split = s.Split(delim);
+            if (i != 0)
+            {
+                if (split[2] == name)
+                {
+                    pos = i;
+                    MessageBox.Show($"Paese trovato in posizione {i}");
+                    con = 0;
+                }
+            }
+            i++;
+        }
+        if (con == -1)
+        {
+            MessageBox.Show("Nessun paese trovato");
+        }
+        sr.Close();
     }
 }
