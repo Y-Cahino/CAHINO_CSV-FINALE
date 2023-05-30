353784dcd9495783a288b61079e69a3321a28d8b
Yan Nascimento <yanzebedeu@hotmail.com>
2023-05-31 01:13:11 +0200

    Aggiungere i file di progetto.

---
+++ CAHINO_CSV-FINALE/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CAHINO_CSV_FINALE
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Punto di ingresso principale dell'applicazione.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
